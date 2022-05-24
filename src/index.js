929fb1a0bb67cf8988614528f4506702fcb31bbd
Cyrus <cyrusyong1@gmail.com>
2022-05-24 15:56:31 -0400

    Routes implemented

--- src/index.js
+++ src/index.js
@@ -4,18 +4,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
-import Home from "./pages/home";
-import About from "./pages/about";
-import Contact from "./pages/contact";
+import About from "./pages/about/about";
+import Contact from "./pages/contact/contact";
+import Calculus from "./pages/calculus/calculus";
+import Physics from "./pages/physics/physics";
+import AllSubjects from "./pages/allSubjects/allSubjects";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/allSubjects" element={<AllSubjects />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/calculus" element={<Calculus />} />
+      <Route path="/physics" element={<Physics />} />
     </Routes>
   </BrowserRouter>
 );
