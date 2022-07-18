b2e33376dd1b1673e023c7cdb391bd071f63d31d
Cyrus <cyrusyong1@gmail.com>
2022-07-17 23:06:02 -0400

    removed the bad stuff

--- src/pages/allSubjects/allSubjects.jsx
+++ src/pages/allSubjects/allSubjects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../../basics/navbar.jsx";
-import { Link, Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function AllSubjects() {
   return (
--- src/pages/calculus/units/1.1/calc_unit_1.1.jsx
+++ src/pages/calculus/units/1.1/calc_unit_1.1.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import test from "./Untitled.png"
 
 export default function CALC_UNIT_1_1() {
   return (
